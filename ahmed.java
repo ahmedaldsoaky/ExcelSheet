343e6559831a38bf4b9c0e16852583deed477d38
AhmedFawzy10 <aaldsoaky10@gmail.com>
2024-06-19 22:35:46 +0300

    Adding modified

--- ahmed.java
+++ ahmed.java
@@ -1,5 +1,9 @@
 class Ahmed{
 public static void main(String []args){
-System.out.println("Ahmed loves yellow");
+	System.out.println("Ahmed loves yellow");
+	int i = 5;
+	int j = 10;
+	int z = i + j;
+	System.out.print(z);
 }
 }
